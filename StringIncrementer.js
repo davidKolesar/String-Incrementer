7efc2af2046f5dc016c5bffa73b73fcca698b3ac
unknown <djkolesa@gmail.com>
2019-11-18 20:44:50 -0800

    David Kolesar is fixing formatting

--- StringIncrementer.js
+++ StringIncrementer.js
@@ -1,104 +1,106 @@
+/**
+ * Function to increment numbers on the end of a string 
+ * - argument: String to be incremented.
+
+ * @typedef {{argument: String}}
+ * @public
+ */
 function incrementString (argument) 
 {
 
-//check if parameter is null
-if(argument === null) 
-{
-console.log("return null");
-  return 1;
-}
+	//check if parameter is null
+	if(argument === null) 
+	{
+		console.log("Parmeter is null");
+		return 1;
+	}
 
-//check if parameters contain numbers
-var chars = argument.split('');
-var containsNumber = false;
-var numbersWithinString = [];
-var numbersToReturn = [];
-var lettersWithinString = [];
-var incrementDigits = false;
-
-
-for(var i = 0; i < chars.length; i++) 
-{
-  if(isNaN(parseFloat(chars[i])))
-  {
-      lettersWithinString.push(chars[i]);
-  } 
-  else 
-  {
-    containsNumber = true;
-    numbersWithinString.push(chars[i]);
-  }
-}
-
-  if(containsNumber === false)
-  {
-   return argument + 1;
-  } 
- 
- var preceedingZeros = 0;
- 
-//handle adding with preceeding digits of zeros
-for(var i = 0; i < numbersWithinString.length; i++)
-{
-console.log("counting zeros in " + numbersWithinString[i]);
-//count zeros
-if(numbersWithinString[i] === "0") 
-{
-console.log("Here is a zero!");
-   preceedingZeros++;
-} 
-
-}
-
-//handle if all zeros
-if(preceedingZeros === numbersWithinString.length)
-{
-  console.log("All of the numbers are zero");
-  preceedingZeros--;
-  
-  var argumentSansLastZero = argument.substring(0, argument.length - 1);
-  return argumentSansLastZero + 1;
-}
+	//check if parameter contains numbers
+	var chars = argument.split('');
+	var containsNumber = false;
+	var numbersWithinString = [];
+	var numbersToReturn = [];
+	var lettersWithinString = [];
+	var incrementDigits = false;
 
 
-//add numbers
-var argumentNumberWithCommas = numbersWithinString.toString();
-var argumentNumberWithoutCommas = argumentNumberWithCommas.replace(/\,/g,"");
-var argumentNumber = parseInt(argumentNumberWithoutCommas);
+	for(var i = 0; i < chars.length; i++) 
+	{
+	  if(isNaN(parseFloat(chars[i])))
+	  {
+		  lettersWithinString.push(chars[i]);
+	  } 
+	  else 
+	  {
+		containsNumber = true;
+		numbersWithinString.push(chars[i]);
+	  }
+	}
 
-//count digits
-var originalDigits = argumentNumber.toString().length;
+	if(containsNumber === false)
+	{
+	  return argument + 1;
+	} 
+	 
+	var preceedingZeros = 0;
+	 
+	//handle adding with preceeding digits of zeros
+	for(var i = 0; i < numbersWithinString.length; i++)
+	{
+		console.log("counting zeros in " + numbersWithinString[i]);
+		//count zeros
+		if(numbersWithinString[i] === "0") 
+		{
+			console.log("Here is a zero!");
+			preceedingZeros++;
+		} 
 
-//add 1 to argument
-argumentNumber++;
+	}
 
-//count again
-var newDigits = argumentNumber.toString().length;
+	//handle if all zeros
+	if(preceedingZeros === numbersWithinString.length)
+	{
+	  console.log("All of the numbers are zero");
+	  preceedingZeros--;
+	  
+	  var argumentSansLastZero = argument.substring(0, argument.length - 1);
+	  return argumentSansLastZero + 1;
+	}
 
-if(originalDigits < newDigits)
-{
-  incrementDigits = true;
-}
+	//add numbers
+	var argumentNumberWithCommas = numbersWithinString.toString();
+	var argumentNumberWithoutCommas = argumentNumberWithCommas.replace(/\,/g,"");
+	var argumentNumber = parseInt(argumentNumberWithoutCommas);
 
+	//count number of digits
+	var originalDigits = argumentNumber.toString().length;
 
-//combine String of argument with numbers
-var argumentLettersWithCommas = lettersWithinString.toString();
-var argumentLettersWithoutCommas = argumentLettersWithCommas.replace(/\,/g,"");;
-var allPreceedingZeros = "";
+	//add 1 to argument
+	argumentNumber++;
 
+	//count new number of digits
+	var newDigits = argumentNumber.toString().length;
 
-//check if digits increased
-if(incrementDigits) 
-{
-  preceedingZeros--;
-}
+	if(originalDigits < newDigits)
+	{
+	  incrementDigits = true;
+	}
 
+	//combine String of argument with numbers
+	var argumentLettersWithCommas = lettersWithinString.toString();
+	var argumentLettersWithoutCommas = argumentLettersWithCommas.replace(/\,/g,"");;
+	var allPreceedingZeros = "";
 
+	//check if digits increased
+	if(incrementDigits) 
+	{
+	  preceedingZeros--;
+	}
 
-for(var i = 0; i < preceedingZeros; i++)
-{
-  allPreceedingZeros = allPreceedingZeros + "0";
-}
- return argumentLettersWithoutCommas + allPreceedingZeros.toString() + argumentNumber;
- 
+	for(var i = 0; i < preceedingZeros; i++)
+	{
+	  allPreceedingZeros = allPreceedingZeros + "0";
+	}
+	
+	 return argumentLettersWithoutCommas + allPreceedingZeros.toString() + argumentNumber; 
 }
